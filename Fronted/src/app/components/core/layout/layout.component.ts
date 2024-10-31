6dfafbc8a6610db04dcaf8416f39b008d8be7466
ctrianaCorrosioncic <ctriana@corrosioncic.com>
2024-10-31 17:52:47 -0500

    creacion grafica ventas

--- Fronted/src/app/components/core/layout/layout.component.ts
+++ Fronted/src/app/components/core/layout/layout.component.ts
@@ -1,33 +1,19 @@
-import { Component, inject, ViewChild } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { AvatarModule } from 'primeng/avatar';
-import { ButtonModule } from 'primeng/button';
-import { RippleModule } from 'primeng/ripple';
-import { SidebarModule } from 'primeng/sidebar';
-import { StyleClassModule } from 'primeng/styleclass';
-import { MenuModule } from 'primeng/menu';
-import { MenubarModule } from 'primeng/menubar';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AccordionModule } from 'primeng/accordion';
-import { environment } from '../../../../environments/environment';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule, RouterModule, MenubarModule, 
-    MenuModule, CommonModule, AccordionModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-
-menuItem : any[] = [];
  
 
 ngOnInit(): void {
-    const menu = Object.assign([], environment.menu);
-    this.menuItem = menu;
-    
+
     const Mostrarmenu = (headerToggle: string, navbarId: string) => {
       const toggleBtn = document.getElementById(headerToggle);
       const nav = document.getElementById(navbarId);
