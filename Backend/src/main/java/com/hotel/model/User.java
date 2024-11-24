6f927d1318aa5a37fc64f867df99978ccd2db080
Ederson <jjarbitrobal@gmail.com>
2024-11-24 15:40:18 -0500

    productos

--- Backend/src/main/java/com/hotel/model/User.java
+++ Backend/src/main/java/com/hotel/model/User.java
@@ -12,6 +12,7 @@ public class User {
     @Id
     private String id;
     private String userName;
+    private String password;
     private String name;
     private String lastName;
     private String fullName;
